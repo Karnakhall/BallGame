9cd65427cb56d6c419909195fe0da4bd3a4955fe
Karnakhall <emil_q21@wp.pl>
2025-08-16 20:33:44 +0200

    Initial commit

---
+++ Source/BallGame/Public/GameMode/BallGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BallGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLGAME_API ABallGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
