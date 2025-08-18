91600cbde8911a1babf60036999516bbc1ec9eff
Karnakhall <emil_q21@wp.pl>
2025-08-18 17:15:22 +0200

    update

--- Source/BallGame/Private/AttributeSet/BallAttributeSet.cpp
+++ Source/BallGame/Private/AttributeSet/BallAttributeSet.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AttributeSet/BallAttributeSet.h"
+#include "AbilitySystem/AttributeSet/BallAttributeSet.h"
 
